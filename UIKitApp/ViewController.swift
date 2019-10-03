81449b53e3521bb06a74a8c14650090964c9f524
Dennis Nesanoff <nndennisnn@gmail.com>
2019-10-03 23:51:04 +0300

    Added segmentedControl, slider, textField and alert

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -9,12 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Setup mail label
+        mainLabel.font = mainLabel.font.withSize(25)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        // Setup segmented control
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
+        // Setup slider
+        slider.value = 1
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .green
+        
+        mainLabel.text = String(slider.value)
     }
 
-
+    @IBAction func changeSegment() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The first segment is selected"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "The second segment is selected"
+            mainLabel.textColor = .green
+        case 2:
+            mainLabel.text = "The third segment is selected"
+            mainLabel.textColor = .orange
+        default:
+            break
+        }
+    }
+    
+    @IBAction func changeSlider() {
+        let backgroundColor = view.backgroundColor
+        view.backgroundColor = backgroundColor?.withAlphaComponent(CGFloat(slider.value))
+        mainLabel.text = String(slider.value)
+    }
+    
+    @IBAction func doneButtonPressed() {
+        guard let inputText = textField.text, !inputText.isEmpty else { return }
+        if let _ = Double(inputText) {
+            showAlert(title: "Wrong format", message: "Please, input your name!")
+        } else {
+            mainLabel.text = textField.text
+            textField.text = nil
+        }
+    }
 }
 
+extension ViewController {
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default) { _ in
+                self.textField.text = nil
+        }
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
+}
