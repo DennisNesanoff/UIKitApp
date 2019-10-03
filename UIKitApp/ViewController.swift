fdc3bf276b19d566f83e704f2c9c27f37eb48c53
Dennis Nesanoff <nndennisnn@gmail.com>
2019-10-03 07:33:33 +0300

    Initial Commit

---
+++ UIKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Dennis Nesanoff on 03/10/2019.
+//  Copyright © 2019 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
